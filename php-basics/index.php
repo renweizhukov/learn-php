d72ba52ab418130d9a718ff48950317d301e3e91
Wei Ren <wrn@meta.com>
2025-06-17 08:27:40 -0700

    [PHP Basics] Data types: Diving into arrays
    https://www.youtube.com/watch?v=HRqrFuQd1Y4&list=PLfdtiltiRHWHjTPiFDRdTOPtSyYfz3iLW&index=11

--- php-basics/index.php
+++ php-basics/index.php
@@ -1,3 +1,22 @@
 <?php
 
-echo '<a href="https://www.google.com">Google</a>';
+echo '<a href="https://www.google.com">Google</a><br>';
+
+$users = [
+  [
+    'username' => 'alex',
+    'email' => 'alex@codecourse.com',
+  ],
+  [
+    'username' => 'billy',
+    'email' => 'billy@codecourse.com',
+  ],
+];
+
+echo '<pre>', var_dump($users), '</pre>';
+
+echo $users[1]['email'];
+
+foreach ($users as $user) {
+  echo '<pre>', var_dump($user), '</pre>';
+}
