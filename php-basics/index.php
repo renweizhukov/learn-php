dff71b8ee3c337fda70e0aa3ef7d029dbacfc195
Wei Ren <wrn@meta.com>
2025-06-18 22:21:40 -0700

    [PHP Basics] IF statements and operators: IF statement basics
    https://www.youtube.com/watch?v=IE9RASJw7LY&list=PLfdtiltiRHWHjTPiFDRdTOPtSyYfz3iLW&index=14

--- php-basics/index.php
+++ php-basics/index.php
@@ -30,3 +30,21 @@ unset($name);
 echo "Hello $name<br>";
 var_dump(isset($name));
 var_dump($name);
+
+$daysOfWeek = [
+  1 => 'Monday',
+  2 => 'Tuesday',
+  3 => 'Wednesday',
+  4 => 'Thursday',
+  5 => 'Friday',
+  6 => 'Saturday',
+  7 => 'Sunday',
+];
+
+$dayOfWeek = -1;
+
+if (in_array($dayOfWeek, array_keys($daysOfWeek))) {
+  echo $daysOfWeek[$dayOfWeek];
+} else {
+  echo 'Invalid day of week';
+}
