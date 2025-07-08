250268afa2b59439f09ff17ad8043ef8c2008858
Wei Ren <wrn@meta.com>
2025-07-08 07:44:02 -0700

    [PHP Basics] The ternary operator
    https://www.youtube.com/watch?v=Igg2qTLdFFY&list=PLfdtiltiRHWHjTPiFDRdTOPtSyYfz3iLW&index=30

--- php-basics/signin.php
+++ php-basics/signin.php
@@ -1,4 +1,11 @@
 <?php
 
 echo $_POST['username'], '<br>';
-echo $_POST['password'];
+echo $_POST['password'], '<br>';
+
+$balance = 10;
+$available_balance = $balance ?: 0;
+
+echo "You available balance is {$available_balance}." . '<br>';
+
+phpinfo();
